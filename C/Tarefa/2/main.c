b4516b1efb31003c9aad1097a5c5e964ea70f385
freitas-renato <renato.freitas@usp.br>
2018-03-28 18:05:18 -0300

    :ambulance: Arrumar erro tarefa 2

--- C/Tarefa/2/main.c
+++ C/Tarefa/2/main.c
@@ -6,12 +6,12 @@ int main() {
     sensor_t sensors[8];
 
     for (int i = 0; i < 8; i++) {
-        sensors[8].threshold = 250;
+        sensors[i].threshold = 250;
     }
 
     for (;;) { // Loop infinito
         // Leia os valores, trate-os e envie-os aqui
     }
 
-    return 0
+    return 0;
 }

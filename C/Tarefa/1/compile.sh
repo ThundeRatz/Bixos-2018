d9fc8df8b7bbe709ee7c010e2bda0f1233918799
Daniel Nery <danielnso@outlook.com>
2018-03-27 21:16:11 -0300

    Tarefas 1 e 2

---
+++ C/Tarefa/1/compile.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+gcc -c main.c -o main.o -Wall -Wextra
+gcc -o tarefa1 main.o
+
+rm main.o
---
+++ C/Tarefa/2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "sensors.h"
+
+int main() {
+    sensor_t sensors[8];
+
+    for (int i = 0; i < 8; i++) {
+        sensors[8].threshold = 250;
+    }
+
+    for (;;) { // Loop infinito
+        // Leia os valores, trate-os e envie-os aqui
+    }
+
+    return 0
+}

435eb5d369948a9fac7e04a220d81617f857ded7
GiovanniCabral <giovannicmorales@gmail.com>
2018-04-01 03:07:53 -0300

    algumas correções

--- C/Tarefa/2/main.c
+++ C/Tarefa/2/main.c
@@ -11,12 +11,12 @@ int main() {
 
     for (;;) { // Loop infinito
         // Leia os valores, trate-os e envie-os aqui
-        uint8_t byte = 0 << 7;
+        uint8_t byte = 0;
         for (int i = 0; i < 8; i++) {
             sensors[i].leitura = get_value();
             if (sensors[i].leitura > sensors[i].threshold)
                 byte = byte | 1 << (9-i); //colocar o 1 na posição correspondente ao i
-                //considerei o sensor 0 o que está mais na esquerda e o 8 o mais na direita
+                //considerei o sensor 0 o que está mais na esquerda e o 7 o mais na direita
         }
         send_data(byte);   
     }

bd7d02d511250c60267f5be281c94c5fb7ebaf57
VictorYamada <v_yamada@hotmail.com>
2018-04-02 19:03:25 -0300

    Tarefa 2

--- C/Tarefa/2/main.c
+++ C/Tarefa/2/main.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
-
 #include "sensors.h"
 
 int main() {
     sensor_t sensors[8];
-
+    int enviar = 0; 
     for (int i = 0; i < 8; i++) {
         sensors[8].threshold = 250;
     }
 
     for (;;) { // Loop infinito
         // Leia os valores, trate-os e envie-os aqui
+        for (int i = 0; i < 8; i++){
+            sensors[i].leitura = get_value(); //Pega os valores
+            if (sensors[i].leitura > sensors[i].threshold){   // valor válido
+                enviar = enviar + 1 << i;
+            }
+            else {                                          // não válido
+                enviar = enviar + 0 << i;
+            }
+            
+        }
+        send_data(enviar);
     }
 
-    return 0
+    return 0;
 }

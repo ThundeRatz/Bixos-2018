e3d4784e8e8ecb875d1b40cc4aca98bf92b6ba9d
diegojsk <diego.jun@thunderatz.org>
2018-04-02 00:34:57 -0300

    tarefa1

--- C/Tarefa/1/main.c
+++ C/Tarefa/1/main.c
@@ -1,7 +1,7 @@
-#import <stdio.h>
-#import <stdlib.h>
-#import <string.h>
-#import <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 #define MAXLINHA   400
 #define MAXCOLUNA  400
@@ -15,6 +15,7 @@
 
 #ifdef max
 #undef max
+#endif
 
 int copia[MAXLINHA][MAXCOLUNA];
 
@@ -50,13 +51,13 @@ void filtroBorda1(int m[MAXLINHA][MAXCOLUNA], int linhas, int colunas, int largu
 void filtroBorda2(int m[MAXLINHA][MAXCOLUNA], int linhas, int colunas, int larguraJanela, int k);
 void filtroBorda3(int m[MAXLINHA][MAXCOLUNA], int linhas, int colunas, int larguraJanela, int k);
 
-void main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     char prefixo_arquivo_out[MAX_NAME - 4];
     int imagem[MAXLINHA][MAXCOLUNA];
     int linhas, colunas, maiorValor, check, larguraJanela, k, limiteValido = 0;
     const char comandos[] = "nrvhedmz123xctgCas";
     char comando = 0;
-    int flag = 0
+    int flag = 0;
 
     if (argc < 2) {
         printf("Usage: %s <arquivo entrada>\n", argv[0]);
@@ -88,7 +89,7 @@ void main(int argc, char *argv[]) {
                     printf("Digite o tamanho da janela: ");
                     scanf("%d", &larguraJanela);
                     if (larguraJanela < JANELA_MIN || larguraJanela > JANELA_MAX) {
-                        printf("Tamanho invalido. A janela deve ser entre %d e %d.\n", JANELA_MIN);
+                        printf("Tamanho invalido. A janela deve ser entre %d e %d.\n", JANELA_MIN,JANELA_MAX);
                     }
                 }
             }
@@ -96,21 +97,21 @@ void main(int argc, char *argv[]) {
 
         if (comando == '1' || comando == '2' || comando == '3') {
             printf("Informe o valor de k: ");
-            scanf("%d", &k)
+            scanf("%d", &k);
         }
 
-        if (comando = 'n') negativo(imagem, linhas, colunas);
-        if (comando = 'r') rotacao(imagem, &linhas, &colunas);
-        if (comando = 'v') rebatimentoVertical(imagem, linhas, colunas);
-        if (comando = 'h') rebatimentoHorizontal(imagem, linhas, colunas);
-        if (comando = 'e') filtroErosao(imagem, linhas, colunas, larguraJanela);
-        if (comando = 'd') filtroDilatacao(imagem, linhas, colunas, larguraJanela);
-        if (comando = 'm') filtroMediana(imagem, linhas, colunas, larguraJanela);
-        if (comando = 'z') filtroMedia(imagem, linhas, colunas, larguraJanela);
-        if (comando = '1') filtroBorda1(imagem, linhas, colunas, larguraJanela, k);
-        if (comando = '2') filtroBorda2(imagem, linhas, colunas, larguraJanela, k);
-        if (comando = '3') filtroBorda3(imagem, linhas, colunas, larguraJanela, k);
-        if (comando = 'a') menu();
+        if (comando == 'n') negativo(imagem, linhas, colunas);
+        if (comando == 'r') rotacao(imagem, &linhas, &colunas);
+        if (comando == 'v') rebatimentoVertical(imagem, linhas, colunas);
+        if (comando == 'h') rebatimentoHorizontal(imagem, linhas, colunas);
+        if (comando == 'e') filtroErosao(imagem, linhas, colunas, larguraJanela);
+        if (comando == 'd') filtroDilatacao(imagem, linhas, colunas, larguraJanela);
+        if (comando == 'm') filtroMediana(imagem, linhas, colunas, larguraJanela);
+        if (comando == 'z') filtroMedia(imagem, linhas, colunas, larguraJanela);
+        if (comando == '1') filtroBorda1(imagem, linhas, colunas, larguraJanela, k);
+        if (comando == '2') filtroBorda2(imagem, linhas, colunas, larguraJanela, k);
+        if (comando == '3') filtroBorda3(imagem, linhas, colunas, larguraJanela, k);
+        if (comando == 'a') menu();
 
         if (comando == 'c') {
             check = 0;
@@ -227,7 +228,7 @@ void menu() {
     printf("s - sair\n\n");
 }
 
-void troca(int a, int b) {
+void troca(int* a, int* b) {
     int aux;
     aux = *a;
     *a = *b;
@@ -302,6 +303,7 @@ posicao compara(int a[MAXLINHA][MAXCOLUNA], int b[MAXLINHA][MAXCOLUNA], int linh
     }
 
     erro.linha = erro.coluna = -2;
+    return erro;
 }
 
 int le_pgm(char* prefixo_do_nome, int m[MAXLINHA][MAXCOLUNA], int *linhas, int* colunas, int* maiorValor) {
@@ -429,7 +431,7 @@ void filtroMediana(int m[MAXLINHA][MAXCOLUNA], int linhas, int colunas, int larg
     for (int i = 0; i < linhas; i++) {
         for (int j = 0; j < colunas; j++) {
             n = 0;
-            for (int k = i - larguraJanela/2, k <= i + larguraJanela/2, k++) {
+            for (int k = i - larguraJanela/2; k <= i + larguraJanela/2; k++) {
                 for (int l = j - larguraJanela/2; l <= j + larguraJanela/2; l++) {
                     if (k >= 0 && l >= 0 && k < linhas && l < colunas) {
                         vizinhanca[n] = copia[k][l];

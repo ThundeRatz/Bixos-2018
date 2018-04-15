ea985bdf756f055a0d5aa7c06a27e0db0d24594c
Drebt <lunafelipe10@gmail.com>
2018-04-15 18:33:29 -0300

    tarefa2

--- C/Tarefa/2/main.c
+++ C/Tarefa/2/main.c
@@ -9,9 +9,18 @@ int main() {
         sensors[i].threshold = 250;
     }
 
-    for (;;) { // Loop infinito
+    for (int j = 0; j < 8; j++) { // Criei um for que vai do valor j=0 até j=7 (equivalente ao for superior)
         // Leia os valores, trate-os e envie-os aqui
+
+        sensors[j].leitura = get_value(); //Pega o dado de cada sensor do 0 ao 7
+        if (sensors[j].leitura > sensors[j].threshold) {//Questiona se esta no threshold
+            send_data(1); //Se sim, envia 1
+        } else {
+            send_data(0);//Se não, envia 0
+        } 
+
     }
+
 
     return 0;
 }

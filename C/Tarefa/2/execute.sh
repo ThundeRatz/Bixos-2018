57a217ec1966919cb4aed05b310cedf1caaa15dc
Marcos Pietrucci <marcospietrucci@gmail.com>
2018-04-15 16:26:18 -0300

    Tarefa 2 de C

--- C/Tarefa/2/execute.sh
+++ C/Tarefa/2/execute.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/bash
+gcc -c main.c sensors.c sensors.h
+gcc -o tarefa2.exe main.c sensors.c sensors.h
+
+rm main.o sensors.o sensors.h.gch
--- C/Tarefa/2/main.c
+++ C/Tarefa/2/main.c
@@ -4,14 +4,22 @@
 
 int main() {
     sensor_t sensors[8];
+    uint8_t data = 0b0;
 
     for (int i = 0; i < 8; i++) {
         sensors[i].threshold = 250;
     }
 
-    for (;;) { // Loop infinito
-        // Leia os valores, trate-os e envie-os aqui
+    for (int j = 0 ; j<=7 ; j++ ) 
+    { 
+        sensors[j].leitura = get_value(); //Lê um valor aleatorio
+        if(sensors[j].leitura > sensors[j].threshold) 
+            data |= (1<<j); //Coloca 1 na posicao do byte correspondente ao sensor
+        else
+            data &= ~(1<<j);
     }
+    //Com o byte já montado, enviar
+    send_data(data);
 
     return 0;
 }

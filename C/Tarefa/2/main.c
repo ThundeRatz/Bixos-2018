065e91fecb09940e2a9ccefed0d10cf8142f3978
Marcos Pietrucci <marcospietrucci@gmail.com>
2018-04-15 18:51:09 -0300

    "Atualização loop infinito - Tarefa2_C"

--- C/Tarefa/2/main.c
+++ C/Tarefa/2/main.c
@@ -5,21 +5,29 @@
 int main() {
     sensor_t sensors[8];
     uint8_t data = 0b0;
+    int j =0;
 
     for (int i = 0; i < 8; i++) {
         sensors[i].threshold = 250;
     }
 
-    for (int j = 0 ; j<=7 ; j++ ) 
+    for (;;) 
     { 
+        if(j == 8)
+        {
+            send_data(data); //Com o byte já montado, enviar
+            j = 0; //Resetar as leituras
+        }
         sensors[j].leitura = get_value(); //Lê um valor aleatorio
         if(sensors[j].leitura > sensors[j].threshold) 
             data |= (1<<j); //Coloca 1 na posicao do byte correspondente ao sensor
         else
             data &= ~(1<<j);
+            
+        j++;
     }
-    //Com o byte já montado, enviar
-    send_data(data);
+    
+    
 
     return 0;
 }

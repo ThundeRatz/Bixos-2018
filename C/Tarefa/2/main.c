664ef8d5d6de4d18acddef9b1ddb44b31beefd4d
Marcos Pietrucci <marcospietrucci@gmail.com>
2018-04-15 14:07:44 -0300

    Tarefa2 v1.0

--- C/Tarefa/2/main.c
+++ C/Tarefa/2/main.c
@@ -4,13 +4,26 @@
 
 int main() {
     sensor_t sensors[8];
+    int j = 0;
+    uint8_t data;
 
     for (int i = 0; i < 8; i++) {
         sensors[i].threshold = 250;
     }
 
-    for (;;) { // Loop infinito
-        // Leia os valores, trate-os e envie-os aqui
+    for (;;) { 
+        if(j == 8) // byte já está montado 
+        {
+            send_data(data);
+            break;
+        }
+        sensors[j].leitura = get_value(); //Recebe valores aleatórios
+        if (sensors[j].leitura > sensors[j].threshold)
+            data |= (1<<j); //montando o byte com valor 1 onde a leitura do "sensor" foi maior que o treshold
+        
+
+
+        j++;
     }
 
     return 0;
